ce0f7ae0f96afa657d113662e484339e5524febd
P1car00n <80679567+P1car00n@users.noreply.github.com>
2024-07-18 12:19:56 +0200

    Implement additional chunkers
    Closes #1

--- src/rag_pipeline/chunking.py
+++ src/rag_pipeline/chunking.py
@@ -4,15 +4,29 @@ All adhere to the strategy pattern as currently implemented.
 """
 
 import abc
+import enum
 import pathlib
 import typing
 
+import langchain_huggingface
 import langchain_text_splitters
-from langchain_core import documents
+from langchain_core import documents, embeddings
+from langchain_experimental.text_splitter import (
+    SemanticChunker as ExperimentalSemanticTextSplitter,
+)
 
 
 class BaseChunker(abc.ABC):
     """Abstract base class defining common chunking operations."""
+
+    @abc.abstractmethod
+    def __init__(self) -> None:
+        """Instantiate the class.
+
+        Args:
+            doc_path: Path to the file to chunk.
+            kwargs: Any arguments to pass to the wrapped object.
+        """
 
     @abc.abstractmethod
     def chunk(self) -> list[documents.Document]:
@@ -24,36 +38,161 @@ class BaseChunker(abc.ABC):
 
 
 class CharacterChunker(BaseChunker):
-    """Splitting text based on a character sequence."""
+    """Split text based on a character sequence.
+
+    It is a thin wrapper over `CharacterTextSplitter`.
+    """
 
     @typing.override
-    def __init__(self, doc_file: pathlib.Path, **kwargs: typing.Any) -> None:
-        super().__init__()
-        self.doc_file = doc_file
+    def __init__(self, doc_path: pathlib.Path, **kwargs: typing.Any) -> None:
+        self.doc_path = doc_path
         self.text_splitter = langchain_text_splitters.CharacterTextSplitter(
             **kwargs,
         )
 
     @typing.override
     def chunk(self) -> list[documents.Document]:
-        with self.doc_file.open() as file:
+        with self.doc_path.open() as file:
             text = file.read()
         return self.text_splitter.create_documents([text])
 
 
 class RecursiveChunker(BaseChunker):
-    """Recursively splitting text based on a character sequence."""
+    """Recursively split text based on a character sequence.
+
+    It is a thin wrapper over `RecursiveCharacterTextSplitter`.
+    """
 
     @typing.override
-    def __init__(self, doc_file: pathlib.Path, **kwargs: typing.Any) -> None:
-        super().__init__()
-        self.doc_file = doc_file
+    def __init__(self, doc_path: pathlib.Path, **kwargs: typing.Any) -> None:
+        self.doc_path = doc_path
         self.text_splitter = (
             langchain_text_splitters.RecursiveCharacterTextSplitter(**kwargs)
         )
 
     @typing.override
     def chunk(self) -> list[documents.Document]:
-        with self.doc_file.open() as file:
+        with self.doc_path.open() as file:
+            text = file.read()
+        return self.text_splitter.create_documents([text])
+
+
+class HTMLChunkingMethod(enum.Enum):
+    """Decide on the type of chunking to perform."""
+
+    HEADER = enum.auto()
+    SECTION = enum.auto()
+
+
+class HTMLChunker(BaseChunker):
+    """Split an HTML document.
+
+    It is a thin wrapper over `HTMLHeaderTextSplitter`
+    and `HTMLSectionSplitter`.
+    """
+
+    @typing.override
+    def __init__(
+        self,
+        method: HTMLChunkingMethod,
+        doc_path: pathlib.Path,
+        **kwargs: typing.Any,
+    ) -> None:
+        """See base class.
+
+        Args:
+            method: Decide how to chunk.
+        """
+        self.doc_path = doc_path
+        match method:
+            case method.HEADER:
+                self.text_splitter = (
+                    langchain_text_splitters.HTMLHeaderTextSplitter(
+                        **kwargs,
+                    )
+                )
+            case method.SECTION:
+                self.text_splitter = (
+                    langchain_text_splitters.HTMLSectionSplitter(
+                        **kwargs,
+                    )
+                )
+
+    @typing.override
+    def chunk(self) -> list[documents.Document]:
+        return self.text_splitter.split_text_from_file(self.doc_path)
+
+
+class MarkdownHeaderChunker(BaseChunker):
+    """Split a Markdown document.
+
+    It is a thin wrapper over `MarkdownHeaderTextSplitter`.
+    """
+
+    @typing.override
+    def __init__(self, doc_path: pathlib.Path, **kwargs: typing.Any) -> None:
+        super().__init__()
+        self.doc_path = doc_path
+        self.text_splitter = (
+            langchain_text_splitters.MarkdownHeaderTextSplitter(
+                **kwargs,
+            )
+        )
+
+    @typing.override
+    def chunk(self) -> list[documents.Document]:
+        with self.doc_path.open() as file:
+            text = file.read()
+        return self.text_splitter.split_text(text)
+
+
+class SemanticChunker(BaseChunker):
+    """Semantically split text.
+
+    It is a thin wrapper over langchain's `SemanticChunker`.
+    """
+
+    @typing.override
+    def __init__(
+        self,
+        doc_path: pathlib.Path,
+        embedding_model: embeddings.Embeddings | str,
+        **kwargs: typing.Any,
+    ) -> None:
+        super().__init__()
+        if isinstance(embedding_model, str):
+            embedding_model = langchain_huggingface.HuggingFaceEmbeddings(
+                model_name=embedding_model,
+            )
+        self.doc_path = doc_path
+        self.text_splitter = ExperimentalSemanticTextSplitter(
+            embedding_model,
+            **kwargs,
+        )
+
+    @typing.override
+    def chunk(self) -> list[documents.Document]:
+        with self.doc_path.open() as file:
+            text = file.read()
+        return self.text_splitter.create_documents([text])
+
+
+class TokenChunker(BaseChunker):
+    """Split text with a hard limit on the token size.
+
+    It is a thin wrapper over `TokenTextSplitter`.
+    """
+
+    @typing.override
+    def __init__(self, doc_path: pathlib.Path, **kwargs: typing.Any) -> None:
+        super().__init__()
+        self.doc_path = doc_path
+        self.text_splitter = langchain_text_splitters.TokenTextSplitter(
+            **kwargs,
+        )
+
+    @typing.override
+    def chunk(self) -> list[documents.Document]:
+        with self.doc_path.open() as file:
             text = file.read()
         return self.text_splitter.create_documents([text])
