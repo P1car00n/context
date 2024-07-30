e2e10b033c434c22eb36ba9c97bddd02fad30d10
P1car00n <80679567+P1car00n@users.noreply.github.com>
2024-07-30 11:28:32 +0200

    Implement additional storage backends

--- src/rag_pipeline/persisting.py
+++ src/rag_pipeline/persisting.py
@@ -7,23 +7,21 @@ import abc
 import typing
 
 import langchain_chroma
-from langchain_core import documents, embeddings, vectorstores
+from langchain_community import vectorstores
+from langchain_core import documents, embeddings
 
 
 class BaseStorage(abc.ABC):
     """Abstract base class defining common storage operations."""
 
-    @abc.abstractmethod
-    def get_vectorstore(self) -> vectorstores.VectorStore:
-        """Return a vector DB with provided embeddings.
-
-        Returns:
-            A VectorStore implementation dependent on the exact class used.
-        """
-
 
 class ChromaStorage(BaseStorage):
-    """Vector storage provided by the Chroma DB."""
+    """Vector storage provided by the Chroma DB.
+
+    Attributes:
+        vectorstore: The wrapped storage object.
+            All of its methods are available to the end user to use.
+    """
 
     @typing.override
     def __init__(
@@ -41,13 +39,73 @@ class ChromaStorage(BaseStorage):
             kwargs: key-word arguments to pass to the underlying storage
                 provider.
         """
-        self.embedding = embedding
+        self._embedding = embedding
         self.vectorstore = langchain_chroma.Chroma.from_documents(
             docs,
-            self.embedding,
+            self._embedding,
             **kwargs,
         )
 
+
+class LanceStorage(BaseStorage):
+    """Vector storage provided by the Lance DB.
+
+    Attributes:
+        vectorstore: The wrapped storage object.
+            All of its methods are available to the end user to use.
+    """
+
     @typing.override
-    def get_vectorstore(self) -> langchain_chroma.Chroma:
-        return self.vectorstore
+    def __init__(
+        self,
+        docs: list[documents.Document],
+        embedding: embeddings.Embeddings,
+        **kwargs: typing.Any,
+    ) -> None:
+        """Instantiate LanceStorage.
+
+        Args:
+            docs: Textual data from which to create embeddings for vector
+                storage.
+            embedding: Model to use to generate embeddings.
+            kwargs: key-word arguments to pass to the underlying storage
+                provider.
+        """
+        self._embedding = embedding
+        self.vectorstore = vectorstores.LanceDB.from_documents(
+            docs,
+            self._embedding,
+            **kwargs,
+        )
+
+
+class FAISSStorage(BaseStorage):
+    """Vector storage provided by the FAISS DB.
+
+    Attributes:
+        vectorstore: The wrapped storage object.
+            All of its methods are available to the end user to use.
+    """
+
+    @typing.override
+    def __init__(
+        self,
+        docs: list[documents.Document],
+        embedding: embeddings.Embeddings,
+        **kwargs: typing.Any,
+    ) -> None:
+        """Instantiate FAISSStorage.
+
+        Args:
+            docs: Textual data from which to create embeddings for vector
+                storage.
+            embedding: Model to use to generate embeddings.
+            kwargs: key-word arguments to pass to the underlying storage
+                provider.
+        """
+        self._embedding = embedding
+        self.vectorstore = vectorstores.FAISS.from_documents(
+            docs,
+            self._embedding,
+            **kwargs,
+        )
