81ca73772940902eb72c986f3434b1321761001d
P1car00n <80679567+P1car00n@users.noreply.github.com>
2024-08-02 15:05:37 +0200

    Implement quality metrics

--- src/rag_pipeline/quality_metrics.py
+++ src/rag_pipeline/quality_metrics.py
@@ -6,7 +6,7 @@ All adhere to the strategy pattern as currently implemented.
 import abc
 import typing
 
-from parea.evals import general
+from parea.evals import general, rag
 from parea.schemas import log
 
 
@@ -14,24 +14,84 @@ class BaseEvaluation(abc.ABC):
     """Abstract base class defining common evaluation operations."""
 
     @abc.abstractmethod
+    @typing.override
+    def __init__(self, **kwargs: typing.Any) -> None:
+        """Instantiate the class.
+
+        Args:
+            kwargs: Key-word arguments to pass to Parea SDK.
+        """
+        self._log = log.Log()
+
+    @abc.abstractmethod
     def evaluate(self) -> float:
-        """Return a grade representing the quality of the pipeline..
+        """Return a grade representing the quality of the pipeline.
 
         Returns:
-            A numerical representation of quality. From 1 to 10,
-                where 10 is the best.
+            A numerical representation of quality. From 0 to 1,
+                where 1 is the best.
         """
 
 
-class LLMGraderEval(BaseEvaluation):
-    """Evaluation based on using an LLM as a judge."""
+class RAGAsEval(BaseEvaluation):
+    """Evaluation based on answer_relevancy_factory."""
 
     @typing.override
-    def __init__(self, model_name: str) -> None:
-        # TODO: add docs.
-        self._llm_factory = general.llm_grader_factory(model_name)
-        # TODO: Figure out how to better set the log object.
-        self._log = log.Log()
+    def __init__(self, **kwargs: typing.Any) -> None:
+        self._llm_factory = general.answer_relevancy_factory(**kwargs)
+        super().__init__()
+
+    @typing.override
+    def evaluate(self) -> float:
+        return self._llm_factory(self._log)
+
+
+class LLMGraderEval(BaseEvaluation):
+    """Evaluation based on llm_grader_factory."""
+
+    @typing.override
+    def __init__(self, **kwargs: typing.Any) -> None:
+        self._llm_factory = general.llm_grader_factory(**kwargs)
+        super().__init__()
+
+    @typing.override
+    def evaluate(self) -> float:
+        return self._llm_factory(self._log)
+
+
+class SelfCheckEval(BaseEvaluation):
+    """Evaluation based on self_check."""
+
+    @typing.override
+    def __init__(self) -> None:
+        super().__init__()
+
+    @typing.override
+    def evaluate(self) -> float:
+        _result = general.self_check(self._log)
+        return _result if _result is not None else 0
+
+
+class LLMJudgeEval(BaseEvaluation):
+    """Evaluation based on lm_vs_lm_factuality_factory."""
+
+    @typing.override
+    def __init__(self, **kwargs: typing.Any) -> None:
+        self._llm_factory = general.lm_vs_lm_factuality_factory(**kwargs)
+        super().__init__()
+
+    @typing.override
+    def evaluate(self) -> float:
+        return self._llm_factory(self._log)
+
+
+class ListwiseRerankingEval(BaseEvaluation):
+    """Evaluation based on context_ranking_listwise_factory."""
+
+    @typing.override
+    def __init__(self, **kwargs: typing.Any) -> None:
+        self._llm_factory = rag.context_ranking_listwise_factory(**kwargs)
+        super().__init__()
 
     @typing.override
     def evaluate(self) -> float:
