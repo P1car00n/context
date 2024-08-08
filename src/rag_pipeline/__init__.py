4ab81ddf3dd173f323b469c5a04bf7ee8d94ab9f
P1car00n <80679567+P1car00n@users.noreply.github.com>
2024-08-08 17:44:13 +0200

    Add helper for text documents

--- src/rag_pipeline/__init__.py
+++ src/rag_pipeline/__init__.py
@@ -18,7 +18,8 @@ from . import (
 
 dotenv.load_dotenv()
 
-_PDF_PATH = "/Users/af/Development/thesis/context/src/tests/resources/documents/Economic Policy Thoughts for Today and Tomorrow.pdf"
+PDF_PATH = "/Users/af/Development/thesis/context/src/tests/resources/documents/Economic Policy Thoughts for Today and Tomorrow.pdf"
+TXT_PATH = "/Users/af/Development/thesis/context/src/tests/resources/documents/Economic Policy Thoughts for Today and Tomorrow.txt"
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -29,21 +30,33 @@ logging.basicConfig(
 )
 
 
-def _get_quality_metrics(
-    query: str, output: str
+def get_text_loader() -> loading.FileSystemLoader:
+    """Helper function returning an instance of FileSystemLoader."""
+    return loading.FileSystemLoader(
+        "/Users/af/Development/thesis/context/src/tests/resources/documents/",
+        glob="*.txt",
+        use_multithreading=True,
+    )
+
+
+def get_quality_metrics(
+    query: str,
+    output: str,
 ) -> list[quality_metrics.BaseEvaluation]:
-    _model = "gpt-4o-mini-2024-07-18"
+    """Return a list of instantiated quality metrics."""
+    model = "gpt-4o-mini"
     return [
-        quality_metrics.RAGAsEval(query, output, model=_model),
-        quality_metrics.LLMGraderEval(query, output, model=_model),
+        quality_metrics.RAGAsEval(query, output, model=model),
+        quality_metrics.LLMGraderEval(query, output, model=model),
         # quality_metrics.SelfCheckEval(),
-        quality_metrics.LLMJudgeEval(query, output, examiner_model=_model),
-        # quality_metrics.ListwiseRerankingEval(query, output, model=_model),
+        quality_metrics.LLMJudgeEval(query, output, examiner_model=model),
+        # quality_metrics.ListwiseRerankingEval(query, output, model=model),
     ]
 
 
-def main():
-    _queries = [
+def main() -> None:
+    """Launch the pipeline."""
+    queries = [
         "What role does private property play in promoting economic efficiency and resource allocation?",
         "Explain the merits of market institutions in contrast to the dangers of government intervention.",
         "How does Mises clarify the quantity theory of money? What implications does this theory have for inflation and monetary policy?",
@@ -56,44 +69,44 @@ def main():
         "Summarize Mises's core message about the relationship between liberty, private property, and prosperity.",
     ]
 
-    _loader = loading.PDFLoader(_PDF_PATH)
-    _chunker = chunking.RecursiveChunker(
-        pathlib.Path(_PDF_PATH),
+    loader = get_text_loader()
+    chunker = chunking.RecursiveChunker(
+        pathlib.Path(TXT_PATH),
         chunk_size=500,
         chunk_overlap=50,
         length_function=len,
         is_separator_regex=False,
     )
 
-    _pipeline = pipeline.RAGPipeline(_loader, _chunker)
+    _pipeline = pipeline.RAGPipeline(loader, chunker)
 
-    _loaded_documents = _pipeline.load_documents()
-    _chunked_documents = _pipeline.chunk_documents(_loaded_documents)
-    _persister = persisting.ChromaStorage(
+    loaded_documents = _pipeline.load_documents()
+    chunked_documents = _pipeline.chunk_documents(loaded_documents)
+    persister = persisting.ChromaStorage(
         langchain_huggingface.HuggingFaceEmbeddings(
             model_name="sentence-transformers/all-MiniLM-L6-v2",
         ),
     )
 
-    _pipeline.persister = _persister
-    _vector_store = _pipeline.persist_documents(_chunked_documents)
+    _pipeline.persister = persister
+    vector_store = _pipeline.persist_documents(chunked_documents)
 
-    _retriever = retrieving.StandardRetriever(_vector_store)
-    _pipeline.retriever = _retriever
+    retriever = retrieving.StandardRetriever(vector_store)
+    _pipeline.retriever = retriever
 
-    _lc_retriever = _pipeline.get_retriever()
+    lc_retriever = _pipeline.get_retriever()
 
-    _generator = generating.LLAMAFileGenerator(_lc_retriever)
-    _pipeline.generator = _generator
+    generator = generating.LLAMAFileGenerator(lc_retriever)
+    _pipeline.generator = generator
 
-    for query in _queries:
-        _context = _lc_retriever.invoke(query)
-        logger.info("Context received: %s", _context)
+    for query in queries:
+        context = lc_retriever.invoke(query)
+        logger.info("Context received: %s", context)
 
-        _answer = _pipeline.generate_answer(query)
+        answer = _pipeline.generate_answer(query)
 
-        _pipeline.eveluators = _get_quality_metrics(query, _answer)
-        _quality = _pipeline.evaluate()
+        # _pipeline.eveluators = get_quality_metrics(query, answer)
+        quality = _pipeline.evaluate()
 
-        print(_answer)
-        print(_quality)
+        print(answer)
+        print(quality)
